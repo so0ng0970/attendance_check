3a43c38565fab5ccb4be48a6638f298a86d98e16
soonger3306 <soonger3306@daum.net>
2023-04-05 16:48:51 +0900

    LocationPermission,checkedPermission

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -33,6 +34,26 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
     );
+  }
+
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isLocationEnabled) {
+      return '위치 서비스를 활성화 해주세요';
+    }
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+    if (checkedPermission == LocationPermission.denied) {
+      //권한요청
+      checkedPermission = await Geolocator.requestPermission();
+      if (checkedPermission == LocationPermission.denied) {
+        return '위치 권한을 허가해주세요';
+      }
+    }
+    if (checkedPermission == LocationPermission.deniedForever) {
+      return '앱의 위치 권한을 세팅에서 허가해주세요';
+    }
+    return '위치 권한을 허가해주세요';
   }
 
   AppBar renderAppBar() {
